cb1a2371c77dcdc7deb64fedd99b72885c46a6ba
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-20 11:55:59 +0700

    Initial commit

---
+++ SwiftUI T29/SwiftUI_T29App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_T29App.swift
+//  SwiftUI T29
+//
+//  Created by M Fadli Zein on 20/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_T29App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
